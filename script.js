9ae96e5b2f6c560d3c80f1cd75fbb0d58f2b97a6
Restaurante Casablanca <tmania762@gmail.com>
2026-09-17 09:03:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function abrirSorpresa() {
+function cambiarPantalla(actual, siguiente) {
 
-    alert("❤️ Bea, esto es solo el principio...");
+    document.getElementById(actual)
+        .classList.remove("activa");
+
+    setTimeout(() => {
+
+        document.getElementById(siguiente)
+            .classList.add("activa");
+
+    }, 500);
+}
+
+
+function abrirCarta() {
+
+    cambiarPantalla("inicio", "carta");
+
+}
+
+
+function irHistoria() {
+
+    alert("La historia de Bea está a punto de comenzar ❤️");
 
 }
